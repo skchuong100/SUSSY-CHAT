46c719e93ea4dc35fe0437db9c86284403dcf01e
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-02-19 10:46:44 -0800

    added comments to file server.js

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+// getting required modules
 const express = require("express");
 const path = require("path");
 
+// returning an express object
 const app = express();
+
+// creatting a server using the http module on express object, don't ask, don't tell
 const server = require("http").createServer(app);
 
+// initializing socket, once again, don't ask, don't tell
 const io = require("socket.io")(server);
 
+// .user() function loads to be used as middleware, the result is whats inside
 app.use(express.static(path.join(__dirname + "/public")));
 
+// this is where the magic begins, this is where we check the socket connection
 io.on("connection", function (socket) {
+    // this event is for a new user joining the chat, three other events, users leaves, and user sends a message
     socket.on("newuser", function (username) {
+        // emit function sends a message to all the clients, differnert messages for different events
         socket.broadcast.emit("update", username + " joined the conversation");
     });
     socket.on("exituser", function (username) {
@@ -20,4 +29,5 @@ io.on("connection", function (socket) {
     });
 });
 
+// the port we are listening on
 server.listen(3000);  
