a5f43d03f84dc4f17a390c8a5ec98c9a0be54193
Yuwei Du <58442992+winonavv@users.noreply.github.com>
2023-03-03 19:49:51 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//Key
+let keyArray = [];
+
+//plain text
+let plainText = 'whatever the user write';
+//convert string to array
+let textArray = Array.from(plainText);
+console.log(textArray);
+//Generator a random key
+//keyGenerator()
+
+
+//Convert character to binary
+//charToBinary(array[Message.length])
