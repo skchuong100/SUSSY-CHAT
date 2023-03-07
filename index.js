c9e4d20dd85258445f413f1380eeb9002c6523e5
Spencer K Chuong <spencerchuong0@gmail.com>
2023-03-06 16:00:28 -0800

    added key_generator funciton

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ let textArray = Array.from(plainText);
 console.log(textArray);
 //Generator a random key
 //keyGenerator()
-
+function KeyGen(length){
+    let result = [];
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./{}[]^_~`';
+    let counter = 0;
+    while (counter < length){
+        result.push(characters.charAt(Math.random() * characters.length));
+        counter += 1;
+    }
+    return result;
+}
+console.log(KeyGen(textArray.length))
 
 //Convert character to binary
 //charToBinary(array[Message.length])
