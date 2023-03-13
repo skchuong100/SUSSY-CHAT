ab03bdba524bf1473fdb2ee99d7b485df22c7ee0
Spencer K Chuong <spencerchuong0@gmail.com>
2023-03-12 20:59:04 -0700

    Fixed xor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const res = require("express/lib/response");
 let keyArray = [];
 
 //plain text
-let plainText = 'whatever the user write';
+let plainText = 'hello world, my name is Spencer';
 //convert string to array
 let textArray = Array.from(plainText);
 console.log(textArray);
@@ -26,7 +26,11 @@ function Char2Binary(keyid){
     let result = [];
     for (var i = 0; i < keyid.length; i++){
         var x = keyid[i].charCodeAt(0).toString(2);
-        result.push(Array(8-x.length+1).join("0") + x);
+        while (x.length < 7){
+            x = '0' + x;
+        }
+        //result.push(Array(8-x.length+1).join("0") + x);
+        result.push(x);
     }
     return result;
 }
@@ -34,21 +38,36 @@ var key = KeyGen(textArray.length);
 console.log(key);
 let keyBinary = Char2Binary(key);
 let textBinary = Char2Binary(textArray);
-console.log(keyBinary);
+console.log('textBinary');
 console.log(textBinary);
+console.log('keyBinary');
+console.log(keyBinary);
+
 
 
 //XOR
 function xorFunction(binaryText, KeyTextBinary){
     let result = [];
     for(var i = 0; i < binaryText.length; i++){
-        result[i] = binaryText[i]^KeyTextBinary[i];
+        let x = (parseInt(binaryText[i], 2).toString(10) ^ parseInt(KeyTextBinary[i], 2).toString(10)).toString(2);
+        while (x.length < 7){
+            x = '0' + x;
+        }
+        result.push(x);
     }
     return result;
 }
 
+
 //XOR Function(PlainTextBinary, KeyTextBinary) = Encryption
-xorFunction(plainText,keyBinary);
+let cipherBinary = xorFunction(textBinary,keyBinary);
+console.log('cipherBinary');
+console.log(cipherBinary);
 
 //XOR Function(CipherTextBinary, KeyTextBinary) = Decryption
-xorFunction(cipherBinary, keyBinary);
+let decrypt = xorFunction(cipherBinary, keyBinary);
+console.log('Decrypted binary');
+console.log(decrypt);
+let answer= decrypt.map(bin => String.fromCharCode(parseInt(bin, 2))).join(' ');
+console.log('Message');
+console.log(answer);
