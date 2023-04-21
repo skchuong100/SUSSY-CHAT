804c4539bdfe95a58cbc244613365f6b1f3c181f
davidvcsulb <80868017+csulbdavidv@users.noreply.github.com>
2023-04-21 16:39:50 -0700

    working build of encryption/decryption chatrooms
    This commit now allows with encrypted chatrooms, to have the entirety of the functionality of encryption/decryption to the right users. The last things we need are QoL updates which include:
    - Message that appears when your ppk has been added to downloads.
    - In Active Rooms screen, display how many users in that room and if it's encrypted/decrypted.
    - You can't upload anyone else's ppk as your own.
    - Clean up the UI with CSS.
    There's probably more but these are the most important things that come to mind.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const username = os.userInfo().username;
 const amalgamation = require('./amalgamation.js'); // import the amalgamation.js file into server.js
 const am = new amalgamation(); // create instance of amalgamation class so that we can use its encryption functions.
 
-
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
@@ -60,7 +59,7 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomName: req.params.room })
   if(rooms[req.params.room].encryptionKeyRoom){
     let userExistingRoomKey = am.KeyGen() // generate a privateKey for a specific user.
-    writeFile(`C:/Users/${username}/Downloads/privateKey.txt`, userExistingRoomKey, 0) // write a file to user so they can get a privateKey.
+    writeFile(`C:/Users/${username}/Downloads/privateKey.ppk`, userExistingRoomKey, 0) // write a file to user so they can get a privateKey.
   } 
 })
 
@@ -73,7 +72,7 @@ function writeFile(fileName, encryptionKey, count){
   fs.writeFile(fileName, encryptionKey, {flag: "wx"}, function(err) {
     if(err){
       fileName = `privateKey(${count++})`
-      writeFile(`C:/Users/${username}/Downloads/${fileName}.txt`, encryptionKey, count++) //recursion to keep checking next availiable file name in someones directory.
+      writeFile(`C:/Users/${username}/Downloads/${fileName}.ppk`, encryptionKey, count++) //recursion to keep checking next availiable file name in someones directory.
     } else{
       console.log('The file has been saved to the Downloads directory.')
     }
@@ -89,13 +88,26 @@ io.on('connection', socket => {
     // built-in function to join room we want
     socket.join(room)
     // socket.id is unique id given by socket, we assign the key: socket.id and value to be the name of the user
-    rooms[room].users[socket.id] = {name: name}
+    rooms[room].users[socket.id] = {name: name, decryptedPeople : []}
     socket.to(room).broadcast.emit('user-connected', name)
   })
   // waitin for a send-chat-message function call with room, message data
   socket.on('send-chat-message', (room, message) => {
     // emit to the room we are currently with the function chat-message with the following data
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id].name })
+    if(rooms[room].encryptionKeyRoom){
+      const decryptedUsers = rooms[room].users[socket.id].decryptedPeople
+      let encryptedMessage = am.encryption(message, rooms[room].users[socket.id].encryptionKeyUser)
+      let userSockets = Object.keys(rooms[room].users)
+      const nonDecryptedSockets = userSockets.filter(sock => !decryptedUsers.includes(sock));
+      decryptedUsers.forEach(sock => {
+        socket.to(sock).broadcast.emit('chat-message', {message: am.decryption(message, encryptedMessage, rooms[room].users[socket.id].encryptionKeyUser), name : rooms[room].users[socket.id].name})
+      });
+      nonDecryptedSockets.forEach(sock => {
+        socket.to(sock).broadcast.emit('chat-message', {message: encryptedMessage, name: rooms[room].users[socket.id].name})
+      });
+    } else{
+      socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id].name })
+    }
   })
   // waiting for the disconnect
   socket.on('disconnect', () => {
@@ -114,7 +126,6 @@ function getUserRooms(socket) {
   }, [])
 }
 
-
 // trying to get an upload
 app.post('/upload', upload.single('file'), (req, res) => {
   console.log("file uploaded: ", req.file.originalname)
@@ -128,9 +139,25 @@ app.post('/upload', upload.single('file'), (req, res) => {
     }
 
     const fileContents = data.toString()
-    // console.log(fileContents)
-    // console.log(rooms[req.body.roomName].users[req.body.userId])
-    rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser = fileContents
-    console.log(rooms[req.body.roomName])
+    if(rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser){
+      const roomUsers = Object.values(rooms[req.body.roomName].users)
+      roomUsers.forEach(user => {
+        // Check if the user's name matches a specific value
+        if (user.encryptionKeyUser === fileContents) {
+          if(fileContents === rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser){
+            return; //exit when user tries to add themselves to their own encryptionKeyUser field.
+          }
+          const userId = req.body.userId
+          const users = rooms[req.body.roomName].users
+          const userKey = Object.keys(users).find(key => users[key] === users[userId])
+          user.decryptedPeople.push(userKey)
+        }
+      });
+    } else{
+      rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser = fileContents //base case when user doesnt have encryption key.
+      //console.log(rooms[req.body.roomName].users)
+      console.log(rooms[req.body.roomName])
+      console.log('user didnt have encryption key')
+    }
   })
 });
