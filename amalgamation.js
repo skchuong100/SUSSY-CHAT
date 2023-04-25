0cf0a86b0375b949aab36eb9a3bbf3edc7ef5732
Malbrek <119638386+Malbrek@users.noreply.github.com>
2023-04-24 19:43:30 -0700

    Web branch that consists of progress made on the EC2 Instance
    cors module has been downloaded. Changes made to server.js & script.js to socket users together on our domain.

---
+++ amalgamation.js
@@ -0,0 +1,255 @@
+/*
+//Plan a
+class OnePad {
+    constructor(){
+
+    }
+    //Generator a random key
+    AddKey(key){
+        //Set up a variable containing a string of letters, characters, and symbols
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./{}[]^_~`?|@<>;:';
+        //Construct a while loop to until the key is 128 in length
+        while (key.length < 128){
+            //Choose a random index from variable and push it to the end of the key
+            key.push(characters.charAt(Math.random() * characters.length));
+        }
+        return key;
+    }
+
+    //keyGenerator()
+    KeyGen(text, length){
+        //Set up an empty list for the key
+        let partKey = [];
+        //Set up a variable containing a string of letters, characters, and symbols
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./{}[]^_~`?|@<>;:';
+        //Create a variable to act as a counter
+        let counter = 0;
+        while (counter < length){
+            partKey.push(characters.charAt(Math.random() * characters.length));
+            counter += 1;
+        }
+        let result = this.AddKey(partKey);
+        return result;
+    }
+
+    //Convert character to binary
+    //charToBinary(array[Message.length])
+    Char2Binary(keyid){
+        //Create an empty array
+        let result = [];
+        //Create a for loop that will loop the length of the key
+        for (var i = 0; i < keyid.length; i++){
+            //Create a variable that will change the character into a number and then a binary
+            var x = keyid[i].charCodeAt(0).toString(2);
+            //Make the binary 7 in length if needed
+            while (x.length < 7){
+                //Add a 0 to the beginning of the binary
+                x = '0' + x;
+            }
+            //Add to the array
+            result.push(x);
+        }
+        return result;
+    }
+
+    //XOR
+    xorFunction(binaryText, KeyTextBinary){
+        //Create an empty array
+        let result = [];
+        //Create a for loop that will loop the length of the binary text
+        for(var i = 0; i < binaryText.length; i++){
+            //Create a variable and assign the value with the result of an xor between the binary numbers of both binary text and binary-key
+            let x = (parseInt(binaryText[i], 2).toString(10) ^ parseInt(KeyTextBinary[i], 2).toString(10)).toString(2);
+            //Make the binary 7 in length if needed
+            while (x.length < 7){
+                //Add a 0 to the beginning of the binary
+                x = '0' + x;
+            }
+            //Push the variable at the end of the array
+            result.push(x);
+        }
+        return result;
+    }
+}
+
+let input = 'Hi my name is Spencer!';
+let plainText = Array.from(input);
+console.log(plainText);
+const cipher = new OnePad(plainText);
+let key = cipher.KeyGen(plainText, plainText.length);
+let keyBinary = cipher.Char2Binary(key);
+console.log(keyBinary);
+let textBinary = cipher.Char2Binary(plainText);
+console.log('textBinary');
+console.log(textBinary);
+console.log('keyBinary');
+console.log(keyBinary);
+//XOR Function(PlainTextBinary, KeyTextBinary) = Encryption
+let cipherBinary = cipher.xorFunction(textBinary,keyBinary);
+console.log('cipherBinary');
+console.log(cipherBinary);
+console.log(cipherBinary.map(bin => String.fromCharCode(parseInt(bin, 2))).join(' '))
+//XOR Function(CipherTextBinary, KeyTextBinary) = Decryption
+let decrypt = cipher.xorFunction(cipherBinary, keyBinary);
+//console.log('Decrypted binary');
+console.log(decrypt);
+let answer= decrypt.map(bin => String.fromCharCode(parseInt(bin, 2))).join(' ');
+//console.log('Message');
+console.log(answer);
+*/
+
+//Plan B
+class amalgamation{
+    constructor(){
+        //Set up a variable array 
+        this.alpha = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./{}[]^_~`?|@<>;: ');
+    }
+    //Generate a key 
+    KeyGen(){
+        //Set up an empty array
+        let key = [];
+        //Set up a variable string
+        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./{}[]^_~`?|@<>;: '
+        //Generate a key up to 128 in length
+        while (key.length < 128){
+            //Add the randomly chosen character to the end of the array 
+            key.push(characters.charAt(Math.random() * characters.length));
+        }
+        let fullKey = '';
+        for (let x = 0; x < key.length; x++){
+            fullKey += key[x];
+        }
+        //Return the key 
+        return fullKey;
+    }
+    //Encrypt the plaintext
+    encryption(plainText, key){
+        let plainTextArray = Array.from(plainText);
+        let keyArray = Array.from(key);
+        //Set up an empty array 
+        let result = [];
+        //Set up a counter 
+        let counter = 0;
+        //Make a while loop to loop as the length of the plaintext
+        while (counter < plainTextArray.length){
+            //Get the index of the first character of the plaintext plus 1
+            let a = this.alpha.indexOf(plainTextArray[counter]) + 1;
+            //Get the index of the first character of the key plus 1
+            let b = this.alpha.indexOf(keyArray[counter]) + 1;
+            //Add the value of the index together
+            let z = a + b;
+            //Get the length of the message
+            let y =  this.alpha.indexOf(this.alpha[this.alpha.length - 1]) + 1;
+            //Mod the sum 
+            let modify = z%y;
+            //Check if the modulo results in a 0
+            if (modify == 0){
+                //Change the value of the modify to the length of the message 
+                modify = y;
+            }
+            //Subtract the modify by 1
+            let sub = modify - 1;
+            //Push the character at the index of the variable array to the end of the array 
+            result.push(this.alpha[sub]);
+            //Increment the counter
+            counter += 1;
+        }
+        let resultString = '';
+        for (let y = 0; y < result.length; y++){
+            resultString += result[y];
+        }
+        //return the resulting array
+        return resultString;
+    }
+    //Decrypt the cipher text 
+
+    decryption(plainText, cipherText, key){
+        let plainTextArray = Array.from(plainText);
+        let keyArray = Array.from(key);
+        let cipherTextArray = Array.from(cipherText)
+        //create an empty array
+        let result = [];
+        //create a conuter 
+        let counter = 0;
+        //Create a while loop that will loop the length of the cipher text
+        while  (counter < cipherTextArray.length){
+            //Get the index of the first character of the ciphertext plus 1
+            let a = this.alpha.indexOf(cipherTextArray[counter]) + 1;
+            //Get the index of the first character of the key plus 1
+            let b = this.alpha.indexOf(keyArray[counter]) + 1;
+            //Subtract a and b 
+            let z = a - b;
+            //Get the length of the message
+            let y =  this.alpha.indexOf(this.alpha[this.alpha.length - 1]) + 1;
+            //Check if the difference is a 0
+            if (z == 0){
+                //Change the difference to the length of the array 
+                z = this.alpha.indexOf(this.alpha[this.alpha.length - 1]) + 1;
+            }
+            //Subtract again by 1
+            let sub = z - 1;
+            //Check if the difference is less than 0
+            if (sub < 0){
+                //Subtract the difference again by 1
+                let j = z - 1;
+                //Add the length of the variable array to get a positive number 
+                let flip = y + j;
+                //Get the character from the variable array 
+                let test = this.alpha[flip];
+                //Check if the character retrieved matches of the character of the plaintext character 
+                if (plainTextArray[counter] == test){
+                    //Push the character at the end of the array 
+                    result.push(this.alpha[flip]);
+                }
+                else{
+                    //Add by 1
+                    let k = z + 1
+                    //Add the length of the variable array to get a positive number 
+                    let flip = y + k;
+                    //Push the character at the end of the array 
+                    result.push(this.alpha[flip]);
+                }
+            }
+            else{
+                //Push the character at the end of the array 
+                result.push(this.alpha[sub]);
+            }
+            //Increment the counter 
+            counter += 1;
+        }
+        let resultString = '';
+        for (let y = 0; y < result.length; y++){
+            resultString += result[y];
+        }
+        //return the resulting array 
+        return resultString;
+    }
+} 
+module.exports = amalgamation
+
+//let messages = ['Hi, my name is Spencer and her name is Yuwei!', 'Nice to meet you', 'You got the goods?', 'We will meet at the Getty Museum at 9:00 AM'];
+//const cipher = new amalgamation();
+//let key = cipher.KeyGen();
+//console.log(key);
+
+/*
+for (let x = 0; x < messages.length; x++){
+    let plainText = messages[x];
+    console.log('plainText');
+    console.log(plainText);
+    let cipherText = cipher.encryption(plainText, key);
+    console.log('cipherText');
+    console.log(cipherText);
+    let decrypted = cipher.decryption(plainText, cipherText, key);
+    console.log('decrypted text');
+    console.log(decrypted);
+    console.log('');
+    
+}
+*/
+
+
+
+    
+
+
--- server.js
+++ server.js
@@ -1,23 +1,188 @@
-const express = require("express");
-const path = require("path");
-
+// getting the express library
+const express = require('express');
+// creating an express object
+const cors = require('cors');
 const app = express();
-const server = require("http").createServer(app);
-
-const io = require("socket.io")(server);
-
-app.use(express.static(path.join(__dirname + "/public")));
-
-io.on("connection", function (socket) {
-    socket.on("newuser", function (username) {
-        socket.broadcast.emit("update", username + " joined the conversation");
-    });
-    socket.on("exituser", function (username) {
-        socket.broadcast.emit("update", username + " left the conversation");
-    });
-    socket.on("chat", function (message) {
-        socket.broadcast.emit("chat", message);
-    });
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
 });
+const fs = require("fs");
+const fse = require('fs-extra');
+const os = require('os');
+const username = os.userInfo().username;
+const amalgamation = require('./amalgamation.js'); // import the amalgamation.js file into server.js
+const am = new amalgamation(); // create instance of amalgamation class so that we can use its encryption functions.
 
-server.listen(3000);  
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const path = require('path');
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(cors());
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use('/api', cors({
+  origin: 'http://sussychat.com'
+}));
+
+// implement the key
+const rooms = {} // array to contain all the rooms we have at the moment
+
+// default room we are taken too
+app.get('/', (req, res) => {
+  // passing rooms object to ejs file (index file)
+  res.render('index', { rooms: rooms })
+})
+
+// sending post request to create new room
+app.post('/room', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // block checks if roomName is empty, redirects to home if so
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  // checking if the checkbox is clicked
+  if (req.body.roomEncryptionRequired === 'on') {
+    let encryptionKeyRoom = am.KeyGen() // generate a private Key for the room to represent the room is encrypted.
+    rooms[req.body.room] = { encryptionKeyRoom: encryptionKeyRoom, users: {} } // case where room is encrypted with an encryption key field.
+  } else {
+    // create a room object where key: name of room value: users in room
+    rooms[req.body.room] = { users: {} } // case where the room is normal and has no encryption field. 
+  }
+  // redirect person to the room they just created
+  res.redirect(req.body.room)
+  // Send info to client side for new room
+  io.emit('room-created', rooms[req.body.room])
+})
+
+
+// person going a room
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/') // redirect to starting page if someone doesn't enter anything in room box.
+  }
+  res.render('room', { roomName: req.params.room })
+  if (rooms[req.params.room].encryptionKeyRoom) {
+    let userExistingRoomKey = am.KeyGen() // generate a privateKey for a specific user.
+
+    const fileName = `privateKey.ppk`;
+    const downloadDir = path.join(__dirname, 'public', 'downloads', username);
+    const filePath = path.join(downloadDir, fileName);
+    writeFile(filePath, userExistingRoomKey, 0); // write a file to user so they can get a privateKey.
+  }
+})
+
+/*
+* This function will end up writing a file to your downloads file. This function is activated when someone
+* joins or initially creates a room that is encrypted because they will need a file to represent their 
+* privateKey as through asymmetric encryption.
+*/
+function writeFile(fileName, encryptionKey, count) {
+  const downloadsDir = path.join(require('os').homedir(), 'Downloads');
+  const filePath = path.join(downloadsDir, fileName);
+  
+  fs.writeFile(fileName, encryptionKey, { flag: "wx" }, function (err) {
+    if (err) {
+      fileName = `privateKey(${count++})`
+
+      writeFile(path.join(downloadsDir, `${fileName}.ppk`), encryptionKey, count++); //recursion to keep checking next availiable file name in someones directory.
+    } else {
+      console.log('The file has been saved to the Downloads directory.')
+    }
+  })
+}
+// listening on port 3000
+server.listen(3000);
+
+// waiting for a connection between client and server to be made
+io.on('connection', socket => {
+  // waiting for new user request from client
+  socket.on('new-user', (room, name) => {
+    // built-in function to join room we want
+    socket.join(room)
+    // socket.id is unique id given by socket, we assign the key: socket.id and value to be the name of the user
+
+    rooms[room].users[socket.id] = { name: name, decryptedPeople: [] }
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  // waitin for a send-chat-message function call with room, message data
+  socket.on('send-chat-message', (room, message) => {
+    // emit to the room we are currently with the function chat-message with the following data
+    if (rooms[room].encryptionKeyRoom) {
+      const decryptedUsers = rooms[room].users[socket.id].decryptedPeople
+      let encryptedMessage = am.encryption(message, rooms[room].users[socket.id].encryptionKeyUser)
+      let userSockets = Object.keys(rooms[room].users)
+      const nonDecryptedSockets = userSockets.filter(sock => !decryptedUsers.includes(sock));
+      decryptedUsers.forEach(sock => {
+        socket.to(sock).broadcast.emit('chat-message', { message: am.decryption(message, encryptedMessage, rooms[room].users[socket.id].encryptionKeyUser), name: rooms[room].users[socket.id].name })
+      });
+      nonDecryptedSockets.forEach(sock => {
+        socket.to(sock).broadcast.emit('chat-message', { message: encryptedMessage, name: rooms[room].users[socket.id].name })
+      });
+    } else {
+      socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id].name })
+    }
+  })
+  // waiting for the disconnect
+  socket.on('disconnect', () => {
+
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id].name)
+      delete rooms[room].users[socket.id]
+    })
+  })
+})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
+
+// trying to get an upload
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log("file uploaded: ", req.file.originalname)
+
+  // getting the contents of the file
+  const filePath = req.file.path;
+  // console.log(req.body.roomName)
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log("Error reading file: ", err)
+    }
+
+    const fileContents = data.toString()
+    if (rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser) {
+      const roomUsers = Object.values(rooms[req.body.roomName].users)
+      roomUsers.forEach(user => {
+        // Check if the user's name matches a specific value
+        if (user.encryptionKeyUser === fileContents) {
+          if (fileContents === rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser) {
+            return; //exit when user tries to add themselves to their own encryptionKeyUser field.
+          }
+          const userId = req.body.userId
+          const users = rooms[req.body.roomName].users
+          const userKey = Object.keys(users).find(key => users[key] === users[userId])
+          user.decryptedPeople.push(userKey)
+        }
+      });
+    } else {
+      rooms[req.body.roomName].users[req.body.userId].encryptionKeyUser = fileContents //base case when user doesnt have encryption key.
+      //console.log(rooms[req.body.roomName].users)
+      console.log(rooms[req.body.roomName])
+      console.log('user didnt have encryption key')
+    }
+  })
+});
